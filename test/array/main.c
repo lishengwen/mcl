82f8d1f821173bce6e590003e67453e9a65558b8
lsw <lsw@dev.qtz>
2013-02-19 10:37:56 +0800

    add test suite codes

---
+++ test/array/main.c
@@ -0,0 +1,104 @@
+#include <mcl_array.h>
+#include <stdio.h>
+
+
+void print_array(mcl_array *arr)
+{
+	mcl_iter it = MCL_ITER_INITIALIZER;
+	int *info = NULL;
+	MCL_FOREACH(it, arr) {
+		info = (int *)arr->iter_info(&it, arr);
+		printf("%d ", *info);
+	}
+	printf("\n");
+}
+
+void print_size(mcl_array *arr)
+{
+	printf("arr cur_size: %d\n", mcl_array_size(arr));
+}
+
+void print_capacity(mcl_array *arr)
+{
+	printf("arr total_size: %d\n", mcl_array_capacity(arr));
+}
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int *intp = NULL;
+	int ret = 0;
+	mcl_array *arr = mcl_array_new(100);
+	print_array(arr);
+	print_size(arr);
+	print_capacity(arr);
+
+	for (i = 0; i < 20; ++ i) {
+		intp = (int *)malloc(sizeof(int));
+		*intp = i;
+		if (i % 2) {
+			ret = mcl_array_insert(intp, 0, arr);
+		}
+		else {
+			ret = mcl_array_preinsert(intp, 0, arr);
+		}
+
+		if (ret < 0) {
+			printf("insert %d error, ret = %d\n", *intp, ret);
+		}
+		else {
+			printf("insert %d ok, ret = %d\n", *intp, ret);
+		}
+	}
+
+	print_size(arr);
+	print_capacity(arr);
+
+	intp = (int *)mcl_array_at(17, arr);
+	printf("pos 17: %d\n", *intp);
+	intp = (int *)mcl_array_at(0, arr);
+	printf("pos 0: %d\n", *intp);
+	print_array(arr);
+
+	mcl_array_delete_indx(17, arr, MCL_DEF_FREE_FN);
+	mcl_array_delete_obj(intp, arr, MCL_DEF_FREE_FN);
+	print_array(arr);
+
+	mcl_array_delete_range(2, 6, arr, MCL_DEF_FREE_FN);
+	print_array(arr);
+
+	intp = (int *)malloc(sizeof(int));
+	*intp = 100;
+	ret = mcl_array_append(intp, arr);
+	print_array(arr);
+
+	for (i = 1; i <= 10; ++ i) {
+		intp = (int *)malloc(sizeof(int));
+		*intp = i * 100;
+		mcl_array_preappend(intp, arr);
+	}
+	print_array(arr);
+
+	mcl_array_clean(arr, MCL_DEF_FREE_FN);
+	print_array(arr);
+
+	for (i = 1; i <= 10; ++ i) {
+		intp = (int *)malloc(sizeof(int));
+		*intp = i * 13;
+		mcl_array_preinsert(intp, 0, arr);
+	}
+	print_size(arr);
+	print_array(arr);
+
+	mcl_array_delete_range(1, 4, arr, MCL_DEF_FREE_FN);
+	print_array(arr);
+
+	print_capacity(arr);
+	mcl_array_tighten(arr);
+	print_capacity(arr);
+
+	mcl_array_destroy(arr, MCL_DEF_FREE_FN);
+
+	return 1;
+}
+
---
+++ test/list/test_list.c
@@ -0,0 +1,126 @@
+#include <mcl_list.h>
+#include <mcl_iterator.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(void *i, void *j)
+{
+	int *ip = (int *)i;
+	int *jp = (int *)j;
+	if (*ip == *jp) return 1;
+
+	return 0;
+}
+
+static void print_list(mcl_list *listp)
+{
+	int *intp;
+	mcl_iter it = MCL_ITER_INITIALIZER;
+	MCL_FOREACH (it, listp) {
+		intp = (int *)MCL_ITERATOR_INFO(it, listp);	
+		printf("%p = %d\n", intp, *intp);
+	}
+}
+
+int main1()
+{
+	mcl_list * listp = mcl_list_new();
+
+	mcl_iter it = MCL_ITER_INITIALIZER;
+
+	int *i = (int *)malloc(sizeof(*i));
+	int *j = (int *)malloc(sizeof(*j));
+	int *k = (int *)malloc(sizeof(*k));
+	int *l = (int *)malloc(sizeof(*l));
+	int *intp;
+	
+	*i = 1;
+	*j = 2;
+	*k = 3;
+	*l = 4;
+
+	mcl_list_insert(i, listp);
+	mcl_list_insert_tail(j, listp);
+	mcl_list_insert(k, listp);
+	mcl_list_insert_tail(l, listp);
+	print_list(listp);
+
+	for (listp->iter_head(&it, listp); !ITER_NULL(&it);) {
+		intp = (int *)MCL_ITERATOR_INFO(it, listp);	
+		printf("%p = %d\n", intp, *intp);
+		if (*intp == 4) {
+			if (MCL_ITERATOR_ERASE(it, listp)) {
+				printf("erase %d\n", *intp);
+				continue;
+			}
+		}
+
+		listp->iter_next(&it, listp);
+	}
+
+	printf("after print: \n");
+
+	print_list(listp);
+
+	mcl_list_destroy(listp, MCL_DEF_FREE_FN);
+}
+
+int main()
+{
+	mcl_list * listp = mcl_list_new();
+
+	int *i = (int *)malloc(sizeof(*i));
+	int *j = (int *)malloc(sizeof(*j));
+	int *k = (int *)malloc(sizeof(*k));
+	int *l = (int *)malloc(sizeof(*l));
+	
+	*i = 1;
+	*j = 2;
+	*k = 3;
+	*l = 4;
+	int *intp = NULL;
+	int cmpnum = 3;
+	int ret = 0;
+	int cmpnum2 = 1;
+
+	mcl_list_insert(i, listp);
+	mcl_list_insert_tail(j, listp);
+	mcl_list_insert(k, listp);
+	mcl_list_insert_tail(l, listp);
+	print_list(listp);
+	printf("\n");
+
+	ret = mcl_list_delete((void *)&cmpnum2, listp, MCL_DEF_FREE_FN);
+	printf("delete 2 fake, ret=%d\n", ret);
+	print_list(listp);
+	printf("\n");
+
+	ret = mcl_list_del_cmp((void *)j, listp, cmp, MCL_DEF_FREE_FN);
+	printf("delete 2, ret=%d\n", ret);
+	print_list(listp);
+	printf("\n");
+
+	//ret = mcl_list_rm_cmp((void *)&cmpnum, listp, cmp);
+	ret = mcl_list_del_cmp((void *)&cmpnum, listp, cmp, NULL);
+	free(k);
+	printf("delete 3, ret=%d\n", ret);
+	print_list(listp);
+	printf("\n");
+	/*
+	ret = mcl_list_delete((void *)k, listp);
+	printf("delete 3, ret=%d\n", ret);
+	print_list(listp);
+	printf("\n");
+	*/
+
+	//ret = mcl_list_remove(i, listp);
+	ret = mcl_list_delete(i, listp, NULL);
+	free(i);
+	printf("delete 1, ret=%d\n", ret);
+	print_list(listp);
+	printf("\n");
+
+	mcl_list_destroy(listp, MCL_DEF_FREE_FN);
+	return 1;
+}
+
